7049c9f91c9b9f6c150ebac79fe3ff6c3602cdbb
Martin Väth <vaeth@mathematik.uni-wuerzburg.de>
2012-02-19 10:59:47 +0100

    Add demo. Use fixed name. Simplify patching.

--- set_prompt.sh
+++ set_prompt.sh
@@ -7,33 +7,29 @@
 
 # In this example, HOSTTEXT is an additional information about the HOST,
 # and HOSTTEXTSAVE provides the previous such information.
-# In particular, if HOSTTEXTSAVE != HOSTTEXT, we pass an argument to the
-# set_prompt.config to denote that we want special colors
+# In particular, if HOSTTEXTSAVE != HOSTTEXT, we pass the argument "1" to
+# set_prompt.config to denote that we want special colors for a changed host
 
 set_prompt() {
-	[ "${HOSTTEXTSAVE}" = "${HOSTTEXT}" ] || set -- "${@}" 1
 	[ -z "${HOSTTEXT}" ] || set -- -e "(${HOSTTEXT})" "${@}"
+	[ "${HOSTTEXTSAVE}" = "${HOSTTEXT}" ] || set -- "${@}" 1
 	local t
-	t="$(PATH="${PATH}:" . set_prompt "${@}" && echo /)" && PS1="${t%/}"
+	t="$(PATH="${PATH}:." . set_prompt "${@}" && echo /)" && PS1="${t%/}"
 }
 
 # For bash, we patch the above function to add the arguments -b.
 # For broken bash versions, also add the argument -l0
 
 if [ -n "${BASH}" ]
-then	if [ "${BASH_VERSINFO[0]}" -eq 3 ]  && [ "${BASH_VERSINFO[1]}" -eq 1 ] \
+then	eval "$(funcdef="$(declare -f set_prompt)"
+	if [ "${BASH_VERSINFO[0]}" -eq 3 ]  && [ "${BASH_VERSINFO[1]}" -eq 1 ] \
 	&& [ "${BASH_VERSINFO[2]}" -le 17 ] && [ "${BASH_VERSINFO[3]}" -le 1 ]
-	then	# Prompt expansion is buggy in these bash releases. Example:
-		# PS1='$(echo "Strange \[\e[0;32m\]Prompt\[\e[0m\] ")'
-		eval "$(a=$(declare -f set_prompt)
-		b='{'
-		c='{ set -- -bl0 "${@}"
-		'
-		printf '%s' "${a/${b}/${c}}")"
-	else	eval "$(a=$(declare -f set_prompt)
-		b='{'
-		c='{ set -- -b "${@}"
-		'
-		printf '%s' "${a/${b}/${c}}")"
+	then	args='-bl0'
+	else	args='-b'
 	fi
+	find='{'
+	replace="${find}
+set -- ${args} \"\${@}\"
+"
+	printf '%s' "${funcdef/${find}/${replace}}")"
 fi
