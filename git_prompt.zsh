1f857e7bf6d01c9b034ea4f6fcb997a82fa87124
Martin Väth <vaeth@mathematik.uni-wuerzburg.de>
2012-03-08 21:06:10 +0100

    Security issue: Drop privileges for git status. Introduce GIT_UPDATE.

--- git_prompt.zsh
+++ git_prompt.zsh
@@ -4,17 +4,29 @@
 # (C) Martin V\"ath <martin@mvath.de>
 #
 # This script is meant to be sourced by zsh.
-# Then the variables VCSBRANCH and VCSSTATUS are updated at directory changes.
+# Then the variables VCSBRANCH and VCSSTATUS are updated at directory changes,
+# using "git status".
+# If the function GitUpdateUser is defined, it is called before "git status";
+# "git status" is only called if GitUpdateUser returns a zero exit status.
+# Before calling "git status", it is attempted to become user
+# $GIT_UPDATE_USER (default: nobody). Set GIT_UPDATE_USER='' to avoid this.
+# You can (temporarily) avoid the whole functionality by unsetting GIT_UPDATE
+# (or setting it to "false" or an empty value.)
 
 if command -v git >/dev/null 2>&1
 then	# Install only if git is available
 GitUpdate() {
 	VCSBRANCH=''
 	VCSSTATUS=''
+	[[ ${GIT_UPDATE} = [yYtT1:]* ]] || return 0
+	[[ $(whence -w GitUpdateUser) != 'GitUpdateUser: function' ]] \
+		|| GitUpdateUser || return 0
 	local i
 	local -aU a
 	a=()
-	for i in ${(@f)"$(git status --porcelain -sb 2>/dev/null)"}
+	for i in ${(@f)"$(u=${GIT_UPDATE_USER-nobody}
+	[[ -z $u ]] || USERNAME=$u >/dev/null 2>&1
+	git status --porcelain -sb 2>/dev/null)"}
 	do	case $i[2] in
 		('#')	VCSBRANCH=${i[4,$#i]}
 			continue
@@ -31,13 +43,16 @@ GitUpdateChpwd() {
 }
 GitUpdatePrecmd() {
 	if [[ -n "$VCSBRANCH$VCSSTATUS" ]]
-	then	[[ -n $git_update_done ]] && git_update_done='' || GitUpdate
+	then	[[ -n ${git_update_done-n} ]] \
+			&& unset git_update_done || GitUpdate
 	fi
 }
 typeset -aU chpwd_functions
 typeset -aU precmd_functions
 chpwd_functions+=GitUpdateChpwd
 precmd_functions+=GitUpdatePrecmd
-GitUpdate
-git_update_done=''
+VCSBRANCH=''
+VCSSTATUS=''
+GIT_UPDATE=:
+unset git_update_done
 fi
