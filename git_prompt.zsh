c964b61c87c57fffd65c96cc218498e656b0a167
Martin Väth <martin@mvath.de>
2016-08-24 20:04:04 +0200

    Use zsh's vcs_info instead of home-brewn solution

--- git_prompt.zsh
+++ git_prompt.zsh
@@ -2,47 +2,35 @@
 # The above line is only meant for editors
 # (C) Martin V\"ath <martin@mvath.de>
 
-command -v git >/dev/null 2>&1 || return 0
-
-: ${VCSBRANCH=} ${VCSSTATUS=}
-: ${GIT_UPDATE=:} ${GIT_UPDATE_USER=nobody}
-export GIT_UPDATE_USER
-
-GitUpdate() {
-	VCSBRANCH=
-	VCSSTATUS=
-	[[ ${GIT_UPDATE-} = [yYtT1:]* ]] || return 0
-	if command -v GitUpdateUser >/dev/null 2>&1
-	then	GitUpdateUser || return 0
-	fi
-	local -aU a
-	a=()
-	local i
-	for i in ${(@f)"$("$(command -v git_update 2>/dev/null || echo git)" \
-		status --porcelain -sb 2>/dev/null)"}
-	do	case $i[2] in
-		('#')	VCSBRANCH=${i[4,$#i]};;
-		(' ')	a+=$i[1];;
-		(*)	a+=$i[2];;
-		esac
-	done
-	VCSSTATUS=${(j::)a}
+() {
+	local s1 s2
+	s1='%B%F{magenta}:%s:%b'
+	s2='%c%u%m%f'
+	zstyle ':vcs_info:*' actionformats $s1'|%a'$s2
+	zstyle ':vcs_info:*' formats $s1$s2
 }
+# We check for changes except for those repositories where this is too costly:
+# Presumably, these are "linux" and "gentoo"
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*:*:*linux*' check-for-changes false
+zstyle ':vcs_info:*:*:*gentoo*' check-for-changes false
 
-GitUpdateChpwd() {
-	GitUpdate
-	git_update_done=:
-}
+zstyle ':vcs_info:*' stagedstr ':S'
+zstyle ':vcs_info:*' unstagedstr ':U'
+zstyle ':vcs_info:git*:*:*' patch-format ':%a'
+zstyle ':vcs_info:git*:*:*' nopatch-format ':%a'
 
-GitUpdatePrecmd() {
-	if [[ -n ${VCSBRANCH:++}${VCSSTATUS:++} ]]
-	then	[[ -n ${git_update_done-} ]] && git_update_done= || GitUpdate
-	fi
-}
+# If git_update is in the path, use that for git
+if (($+commands[git_update]))
+then	zstyle ':vcs_info:git*:*:*' command $commands[git_update]
+fi
 
-autoload -Uz add-zsh-hook
-add-zsh-hook chpwd GitUpdateChpwd
-add-zsh-hook precmd GitUpdatePrecmd
+# If GIT_UPDATE_USER is not set and we cannot drop permissions to "nobody",
+# we export an empty GIT_UPDATE_USER to make git_update work without attempting
+# to change permissions.
+[[ -n "${GIT_UPDATE_USER++}" ]] || (
+	USERNAME=nobody && [[ $USERNAME == nobody ]]
+) >/dev/null 2>&1 || export GIT_UPDATE_USER=
 
-git_update_done=
-[[ ${1-} = '-n' ]] || GitUpdateChpwd
+autoload -Uz vcs_info add-zsh-hook
+add-zsh-hook precmd vcs_info
